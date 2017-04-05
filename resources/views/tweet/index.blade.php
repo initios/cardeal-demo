5b74996d5acaac9b99f674d9dc3a6dbe8c60e125
Carlos Goce <carlosgoce@gmail.com>
2017-04-05 18:49:38 +0200

    Protect routes

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -1,15 +1,31 @@
-<h2>{{ count($tweets) }} tweets</h2>
+@extends('layouts.app')
 
-<table class="table">
-    @forelse ($tweets as $tweet)
-        @if ($loop->first)
-            <tr><th>id</th><th>contenido</th><th>usuario</th></tr>
-        @endif
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Home</div>
 
-        <tr>
-            <td>{{ $tweet->id }}</td><td>{{ $tweet->content }}</td><td>{{ $tweet->user->name }}</td>
-        </tr>
-    @empty
-        <tr><th>Todavía no hay tweets</th></tr>
-    @endforelse
-</table>
+                <div class="panel-body">
+                <h2>{{ count($tweets) }} tweets</h2>
+
+                <table class="table">
+                    @forelse ($tweets as $tweet)
+                        @if ($loop->first)
+                            <tr><th>id</th><th>contenido</th><th>usuario</th></tr>
+                        @endif
+
+                        <tr>
+                            <td>{{ $tweet->id }}</td><td>{{ $tweet->content }}</td><td>{{ $tweet->user->name }}</td>
+                        </tr>
+                    @empty
+                        <tr><th>Todavía no hay tweets</th></tr>
+                    @endforelse
+                </table>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
