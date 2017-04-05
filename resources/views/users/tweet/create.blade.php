8356e1ae0dde805b897ee0352c475715d4756d3d
Carlos Goce <carlosgoce@gmail.com>
2017-04-05 19:25:12 +0200

    Fix

--- resources/views/users/tweet/create.blade.php
+++ resources/views/users/tweet/create.blade.php
@@ -8,28 +8,18 @@
                 <div class="panel-heading">Crear tweet</div>
 
                 <div class="panel-body">
-                    @if (count($errors) > 0)
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('user.tweet.store', $user->id) }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Contenido</label>
 
                             <div class="col-md-6">
                                 <input id="content" type="text" class="form-control" name="content" value="{{ old('content') }}" autofocus>
 
-                                @if ($errors->has('name'))
+                                @if ($errors->has('content'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                        <strong>{{ $errors->first('content') }}</strong>
                                     </span>
                                 @endif
                             </div>
